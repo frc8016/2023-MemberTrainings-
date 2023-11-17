ed94b5b8dcf621ed1e0fee4f2469b1736b951df4
carolinemerkle <126303090+carolinemerkle@users.noreply.github.com>
2023-11-16 17:02:55 -0800

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,5 +15,59 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+    public static final int JoystickID = 0;
+    public static int JOYSTICK_PORT = 1;
+    public static int JOYSTICK_Y_AXIS = 1;
+    public static int JOYSTICK_X_AXIS = 0;
+  }
+
+  public static class DriveTrainConstants {
+    public static final int frontLeftMotorID = 3;
+    public static final int backLeftMotorID = 1;
+    public static final int frontRightMotorID = 4;
+    public static final int backRightMotorID = 2;
+  }
+
+  public static class ShooterConstants {
+    // left motor
+    public static final int frontShooterId = 7;
+    // right motor
+    public static final int backShooterId = 8;
+
+    public static final double frontShooterSpeed = .5;
+
+    public static final double backIndexSpeed = 1;
+
+    public static final double innerOuterSpeed = 1;
+
+    public static final double innerOuterSpeedReversed = -.1;
+  }
+
+  public static class ArmConstants {
+    // motors
+    public static final int leftArmMotorID = 5;
+    public static final int rightArmMotorID = 6;
+
+    public static final int MOTOR_CURRENT_LIMIT = 20;
+    // encoders
+    public static final int RELATIVE_ENCODER_A = 9;
+    public static final int RELATIVE_ENCODER_B = 7;
+    public static final int ABSOLUTE_ENCODER_PORT = 8;
+
+    public static final double relativeEncoderDistancePerPulse = 2 * Math.PI / 1024;
+    public static final double dutyCycleEncoderDistancePerRotation = 2 * Math.PI;
+
+    public static final double kp = 0.;
+    public static final double ki = 0.;
+    public static final double kd = 0;
+
+    public static final double kMaxVelocityRadPerSecond = Math.PI / 4;
+    public static final double kMaxAccelerationRadPerSecondSquared = 1;
+    public static final double kAngleOfOffset = 0;
+
+    public static final double ks = 0.16993;
+    public static final double kv = 3;
+    public static final double ka = 0.0094614;
+    public static final double kg = 0.15;
   }
 }
