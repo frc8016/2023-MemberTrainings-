0369710c74d9147432ea1bcdbed2e5b19495f59a
carolinemerkle <126303090+carolinemerkle@users.noreply.github.com>
2023-11-16 17:04:37 -0800

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,25 +4,69 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.DriveTrain;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
  */
-public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-    public static final int JOYSTICK_X_AXIS = 0;
-    public static final int JOYSTICK_Y_AXIS = 1;
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+  private final DriveTrain m_DriveTrain = new DriveTrain();
+
+  private final Joystick m_Joystick = new Joystick(0);
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+
+    m_DriveTrain.setDefaultCommand(
+        new RunCommand(
+            () ->
+                m_DriveTrain.arcadeDrive(
+                    m_Joystick.getRawAxis(OperatorConstants.JOYSTICK_Y_AXIS),
+                    m_Joystick.getRawAxis(OperatorConstants.JOYSTICK_X_AXIS)),
+            m_DriveTrain));
   }
 
-  public final class DriveTrainConstants {
-    public static final int FRONT_LEFT_MOTOR_ID = 1;
-    public static final int BACK_LEFT_MOTOR_ID = 2;
-    public static final int FRONT_RIGHT_MOTOR_ID = 3;
-    public static final int BACK_RIGHT_MOTOR_ID = 4;
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    return null;
+    // An example command will be run in autonomous
+
   }
 }
